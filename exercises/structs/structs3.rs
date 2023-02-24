4d1fe30b9d84b38bec0f501c25355f77027cbd04
rizqicy <dev@rizqi.cy>
2023-02-24 19:52:59 +0700

    Revert "answered: structs"
    This reverts commit 9de06dd2b5f30d69c76aeaf3a4485189c148f59d.

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -26,14 +26,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> bool {
+    fn is_international(&self) -> ??? {
         // Something goes here...
-        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+    fn get_fees(&self, cents_per_gram: i32) -> ??? {
         // Something goes here...
-        self.weight_in_grams * cents_per_gram
     }
 }
 
