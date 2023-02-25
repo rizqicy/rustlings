8fa2b75543357a1634b32e82fa2274e8e858060c
rizqicy <dev@rizqi.cy>
2023-02-25 19:19:40 +0700

    answered: hashmaps

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -10,17 +10,21 @@
 //
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    // TODO: declare your hash map here.
+    let mut basket = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("mango"), 1);
+    basket.insert(String::from("papaya"), 1);
+    basket.insert(String::from("watermelon"), 1);
+    basket.insert(String::from("apple"), 1);
 
     basket
 }
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,12 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+
+#[derive(Debug)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -40,6 +41,26 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team2 = scores.entry(team_1_name.clone()).or_insert(
+            Team{
+                name: team_1_name, 
+                goals_scored: 0, 
+                goals_conceded: 0
+            });
+        team2.goals_scored += team_1_score;
+        team2.goals_conceded += team_2_score;
+
+        println!("{:?}",team2);
+        let team = scores.entry(team_2_name.clone()).or_insert(
+            Team { 
+                name: team_2_name, 
+                goals_scored: 0, 
+                goals_conceded: 0 
+            });
+        team.goals_scored += team_2_score;
+        team.goals_conceded += team_1_score;
+
+        println!("{:?}",team);
     }
     scores
 }
